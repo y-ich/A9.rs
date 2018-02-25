49a0e02592329eebbb37ea42c04f2872173d5adc
Yuji Ichikawa <ichikawa.yuji@gmail.com>
2018-02-25 19:53:05 +0900

    could compile wasm

--- rust_pyaq/src/lib.rs
+++ rust_pyaq/src/lib.rs
@@ -6,24 +6,33 @@ extern crate itertools;
 #[macro_use]
 extern crate lazy_static;
 extern crate rand;
+
 #[cfg(not(target_arch = "wasm32"))]
 extern crate sgf; // libcを使うのでwasm32では使わないようにする
+#[cfg(not(target_arch = "wasm32"))]
+extern crate tensorflow;
+
 #[cfg(target_arch = "wasm32")]
 #[macro_use]
 extern crate stdweb;
-#[cfg(not(target_arch = "wasm32"))]
-extern crate tensorflow;
+#[cfg(target_arch = "wasm32")]
+extern crate serde;
+#[cfg(target_arch = "wasm32")]
+#[macro_use]
+extern crate serde_derive;
 
 pub mod utils;
 pub mod numpy;
 pub mod constants;
 pub mod stone_group;
 pub mod board;
+pub mod search;
+
 #[cfg(not(target_arch = "wasm32"))]
 pub mod neural_network;
-pub mod search;
 #[cfg(not(target_arch = "wasm32"))]
 pub mod gtp;
+
 #[cfg(target_arch = "wasm32")]
 mod js_client;
 
@@ -31,15 +40,31 @@ mod js_client;
 use stdweb::js_export;
 
 #[cfg(target_arch = "wasm32")]
+#[derive(Serialize)]
+pub struct MoveInfo {
+    mov: usize,
+    win_rate: f64,
+}
+
+#[cfg(target_arch = "wasm32")]
+js_serializable!(MoveInfo);
+
+#[cfg(target_arch = "wasm32")]
 #[js_export]
-pub fn think(pv: Vec<(usize)>, byoyomi: f64) -> (usize, f64) {
+pub fn think(pv: Vec<usize>, byoyomi: f64) -> MoveInfo {
     use std::f64;
 
     let mut client = js_client::JsClient::new();
     if client.load_pv(&pv).is_ok() {
         let result = client.best_move(byoyomi as f32);
-        (result.0, result.1 as f64)
+        MoveInfo {
+            mov: result.0,
+            win_rate: result.1 as f64,
+        }
     } else {
-        (usize::max_value(), f64::NAN)
+        MoveInfo {
+            mov: usize::max_value(),
+            win_rate: f64::NAN,
+        }
     }
 }
